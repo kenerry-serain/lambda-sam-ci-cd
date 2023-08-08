c618b9aef38359dd0a60ff848683545b9ba02b48
Kenerry Serain <kserain.development@gmail.com>
2023-08-08 18:01:56 -0300

    Uploading Solution

--- sam-app/getAllUsersFunction/tests/unit/test-handler.test.ts
+++ sam-app/getAllUsersFunction/tests/unit/test-handler.test.ts
@@ -1,5 +1,5 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { lambdaHandler } from '../../app';
+import { lambdaHandler as handler } from '../../app';
 import { expect, describe, it } from '@jest/globals';
 
 describe('Unit test for app handler', function () {
@@ -53,7 +53,7 @@ describe('Unit test for app handler', function () {
             resource: '',
             stageVariables: {},
         };
-        const result: APIGatewayProxyResult = await lambdaHandler(event);
+        const result: APIGatewayProxyResult = await handler(event);
 
         expect(result.statusCode).toEqual(200);
         expect(result.body).toEqual(
